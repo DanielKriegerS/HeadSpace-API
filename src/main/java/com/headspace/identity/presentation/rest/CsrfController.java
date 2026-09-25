5d8e3032f49a66a07bb0989ba4628214ae6c5b3b
DanielKriegerS <danielschifelbain@gmail.com>
2026-09-25 11:17:13 -0300

    New: created CSRF Controller

---
+++ src/main/java/com/headspace/identity/presentation/rest/CsrfController.java
@@ -0,0 +1,22 @@
+package com.headspace.identity.presentation.rest;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class CsrfController {
+
+    @GetMapping("/csrf")
+    public Map<String, String> csrf(CsrfToken csrfToken) {
+        return Map.of(
+                "headerName", csrfToken.getHeaderName(),
+                "parameterName", csrfToken.getParameterName(),
+                "token", csrfToken.getToken()
+        );
+    }
+}
